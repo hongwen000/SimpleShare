f2b40baa131b6e2463fd8f0ac764da9d86609553
hongwen000 <hongwen000@outlook.com>
2019-05-08 12:42:09 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,77 @@
+from util import *
+import math
+import os
+import sqlite3
+import sys
+import codecs
+import zlib
+from pathlib import Path
+import xmlrpc
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+with SimpleXMLRPCServer(('localhost', PORT),
+                        requestHandler=RequestHandler) as server:
+    server.register_introspection_functions()
+
+    processing_file = {}
+
+    db = sqlite3.connect('msg.db')
+
+    c = db.cursor()
+
+    c.execute('create table if not exists msg (what text)')
+    db.commit()
+
+    def rpc_send_text(s):
+        print(s)
+        c.execute('insert into msg values (?)', [s])
+        db.commit()
+        return True
+    server.register_function(rpc_send_text)
+
+    @server.register_function
+    def rpc_send_file_open(fn):
+        print("file: ", fn)
+        f = open(fn, 'wb+')
+        processing_file[fn] = f
+        return True
+
+    @server.register_function
+    def rpc_send_file_content(fn, b):
+        f = processing_file[fn]
+        f.write(fileunzip(b))
+        return True
+    
+    @server.register_function
+    def rpc_send_file_close(fn):
+        processing_file[fn].close()
+        return True
+
+
+    @server.register_function
+    def get_update():
+        p = Path('.')
+        c.execute('select * from msg')
+        msgs = [strzip(msg[0]) for msg in c.fetchall()]
+        files = [strzip(f.name) for f in p.iterdir() if not f.is_dir()]
+        return msgs, files
+    
+    @server.register_function
+    def download_file(fn, loc):
+        with open(fn, 'rb') as f:
+            f.seek(loc)
+            return xmlrpc.client.Binary(filezip(f.read(CHUNCK_SIZE)))
+    
+    @server.register_function
+    def get_file_size(fn):
+        return os.path.getsize(fn)
+
+    # Run the server's main loop
+    server.serve_forever()
+    db.close()
